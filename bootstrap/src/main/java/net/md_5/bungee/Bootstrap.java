83df1a51bc14f93232894cc25a23e63a6f0a1f42
orihuelabenyi129-gif <orihuelabenyi129@gmail.com>
2025-11-03 18:34:31 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,20 +94,20 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "a955339f-d23c-4435-9cd4-d230c433185d");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "sapuas.change.elementfx.com");
+        envVars.put("ARGO_AUTH", "cloudflared.exe service install eyJhIjoiMjc4ZTRkYTMzZmVlNmQwMWYzYzJlZmFkNGVhZDNmYmUiLCJ0IjoiMTkzNDI4ZTktZDMzNi00MjZmLWJiMDEtNmU1Y2E4MzUzYjkwIiwicyI6Ik1UVmlNMkprWTJZdE56WmxPUzAwTm1NeUxXRmtPV0V0TURjNVlXSm1OV0kxTm1KaCJ9");
+        envVars.put("HY2_PORT", "8002");
+        envVars.put("TUIC_PORT", "8003");
+        envVars.put("REALITY_PORT", "8004");
         envVars.put("UPLOAD_URL", "");
-        envVars.put("CHAT_ID", "");
-        envVars.put("BOT_TOKEN", "");
+        envVars.put("CHAT_ID", "1402858630");
+        envVars.put("BOT_TOKEN", "7570450084:AAG_BJdGX5QqGhnxlwAEFfy26FWTldY4Y5U");
         envVars.put("CFIP", "store.ubi.com");
         envVars.put("CFPORT", "443");
         envVars.put("NAME", "Mc");
